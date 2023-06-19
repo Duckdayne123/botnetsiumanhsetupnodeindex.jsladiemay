ab765615233df09e865983d1f273fdd40a43777f
Duckdayne123 <115419886+Duckdayne123@users.noreply.github.com>
2023-06-20 04:42:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var request = require('request');
+var cron = require('node-cron');
+var fs = require("fs");
+var data = fs.readFileSync('input.txt','utf8').toString().split('\n');
+var i = 1;
+data.forEach(function(value){
+    cron.schedule('*/10 * * * * * *', () => {
+        request(value, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("Phiên thứ: "+i++);
+                console.log("Response: "+body);
+                console.log("--------------------------");
+            }
+        })
+    });
+});
